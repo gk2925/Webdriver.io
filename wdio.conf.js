215e8b74ed3a714124f0ceb51df8a7ebed0ea34e
Tyagi,Gaurav <Gaurav.Tyagi@clarivate.com>
2021-10-17 10:52:23 +0200

    changes in retry count

--- wdio.conf.js
+++ wdio.conf.js
@@ -105,10 +105,10 @@ exports.config = {
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
-    connectionRetryTimeout: 120000,
+    connectionRetryTimeout: 10000,
     //
     // Default request retries count
-    connectionRetryCount: 3,
+    connectionRetryCount: 1,
     //
     // Test runner services
     // Services take over a specific job you don't want to take care of. They enhance
